4053308f76f3c9d589c7b6807979513a949d1825
jiaolong <zzia615@163.com>
2019-08-20 22:30:55 +0800

    新增Loader

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace GameMapLoader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 21);
+            this.progressBar1.Maximum = 1000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(401, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "正在处理数据，请稍候...";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 79);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "文件更新";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace GameMapLoader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!b_finished)
+                e.Cancel = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 1;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+
+            SetProgress("正在处理文件，请稍候...");
+            string s_path = AppDomain.CurrentDomain.BaseDirectory;
+            string s_dest_path = Path.Combine(s_path, "datas\\NCSData\\");
+            string s_temp_path = Path.Combine(s_path, "temp");
+            string s_file = Path.Combine(s_temp_path, "data.zip");
+            CreateDir(s_temp_path);
+            //WriteDataFile( s_file);
+            SetProgress("正在解压缩文件，请稍候...");
+            Stream stream = GetType().Assembly.GetManifestResourceStream("GameMapLoader.MapData.data.zip");
+
+            ZipInputStream zipStream = new ZipInputStream(stream);
+            ZipEntry entry;
+            while ((entry = zipStream.GetNextEntry()) != null)
+            {
+                string path_name = Path.GetDirectoryName(entry.Name);
+                string file_name = Path.GetFileName(entry.Name);
+                if (path_name != String.Empty)
+                {
+                    string s_zip_path = Path.Combine(s_dest_path, path_name);
+                    CreateDir(s_zip_path);
+                }
+                if (file_name != String.Empty)
+                {
+                    SetProgress("正在解压缩文件" + file_name);
+                    string s_zip_file = Path.Combine(Path.Combine(s_dest_path, path_name), file_name);
+                    WriteDataFile(zipStream, s_zip_file);
+                }
+            }
+            zipStream.Close();
+            FinishProgress();
+
+            b_finished = true;
+            this.Close();
+        }
+
+        private void WriteDataFile(ZipInputStream stream,string s_file)
+        {
+            FileStream fs = File.Open(s_file, FileMode.OpenOrCreate, FileAccess.Write);
+            byte[] bytes = new byte[stream.Length];
+           
+            int readCount = 0;
+            int readLen = stream.Read(bytes, readCount, bytes.Length);
+            fs.Write(bytes, 0, readLen);
+            fs.Flush();
+            fs.Close();
+        }
+
+        void CreateDir(string dir)
+        {
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+        }
+        void DeleteDir(string dir)
+        {
+            //if (Directory.Exists(dir))
+            //{
+            //    Directory.Delete(dir);
+            //}
+        }
+        void SetProgress(string msg)
+        {
+            label1.Text = msg;
+            if (progressBar1.Value < progressBar1.Maximum) {
+                progressBar1.Value += 1;
+            }
+            this.Refresh();
+        }
+        bool b_finished = false;
+        void FinishProgress()
+        {
+            label1.Text = "正在完成，请稍候...";
+            progressBar1.Value = progressBar1.Maximum;
+            this.Refresh();
+            Thread.Sleep(600);
+        }
+    }
+}
